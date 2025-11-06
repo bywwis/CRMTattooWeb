f4b4a0325d9cecbea98502551c0cf421ad096dcb
Dasha-kykyrys <dashulya.art@gmail.com>
2025-11-06 03:06:31 +0300

    Редактирование стилей

--- configdb.py
+++ configdb.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:root@localhost:5432/tattoo'
+        'postgresql://postgres:67admin45@localhost:5432/tattoo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or '1'
